2217939a1c2b3cfadfff16a854e4655432d24dfa
Jon Dilworth <jon@dilworth.dev>
2023-09-22 15:19:54 +0100

    computed filterable properties to assignments

--- js/App.js
+++ js/App.js
@@ -1,45 +1,60 @@
 export default {
     components: {},
     template: `
-        <p>Welcome to the new {{ app }}!</p>
+    <section v-show="filterAssignments.incomplete.length">
+        <h2 class="font-bold mb-2">In Progress</h2>
+
+        <ul>
+            <li 
+                v-for="assignment in filterAssignments.incomplete" 
+                :key="assignment.id"
+            >
+                <label>
+                    {{ assignment.name }} 
+                    
+                    <input type="checkbox" v-model="assignment.complete">
+                </label>
+            </li>
+        </ul>
+
+    </section>
+
+    <section v-show="filterAssignments.completed.length">
+
+        <h2 class="font-bold mt-8">Completed</h2>
+
+        <ul>
+            <li 
+                v-for="assignment in filterAssignments.completed" 
+                :key="assignment.id"
+            >
+                <label>
+                    {{ assignment.name }} 
+                    
+                    <input type="checkbox" v-model="assignment.complete">
+            
+                </label>
+            </li>
+        </ul>
+
+    </section>
     `,
     data() {
         return {
             app: "Application Name",
+            assignments: [
+                { id: 1, name: 'Assignment One', complete: false },
+                { id: 2, name: 'Assignment Two', complete: false },
+                { id: 3, name: 'One More Assignment', complete: false },
+            ],
         };
     },
-    beforeCreate() {
-        console.log("Lifecycle hook: beforeCreate.");
-    },
-    created() {
-        console.log("Lifecycle hook: created.");
-    },
-    beforeMount() {
-        console.log("Lifecycle hook: beforeMount.");
-    },
-    mounted() {
-        console.log("Lifecycle hook: mounted.");
-    },
-    beforeUpdate() {
-        console.log("Lifecycle hook: beforeUpdate.");
-    },
-    updated() {
-        console.log("Lifecycle hook: updated.");
-    },
-    beforeUnmount() {
-        console.log("Lifecycle hook: beforeUnmount.");
-    },
-    unmounted() {
-        console.log("Lifecycle hook: unmounted.");
-    },
-    methods: {
-        myCustomMethod() {
-            let result = "stuff goes here...";
-            console.log(result);
-            return result;
-        },
-        oneMoreMethod() {
-            return null;
+    computed: {
+        filterAssignments() {
+            return {
+                incomplete: this.assignments.filter(x => !x.complete),
+                completed: this.assignments.filter(x => x.complete),
+            };
         },
     },
 }
